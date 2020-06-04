a81493401ce59502cb872ca0fe29992aee665e4b
dsbansel <65608455+dsbansel@users.noreply.github.com>
2020-06-04 15:34:37 +0100

    Update urls.py

--- CV/urls.py
+++ CV/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CV, name='CV'),
+    path('CV', views.CV, name='CV'),
     path('section/<int:pk>/', views.section_detail, name='section_detail'),
     path('section/<int:pk>/edit/', views.section_edit, name='section_edit'),
 ]
