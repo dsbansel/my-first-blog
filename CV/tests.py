2361a077434da975a569a40b140d466b11925142
Davinder <dsbansel2600@gmail.com>
2020-06-14 17:33:59 +0100

    unit and functional tests uploaded in updated form

--- CV/tests.py
+++ CV/tests.py
@@ -1,3 +1,61 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.shortcuts import render, get_object_or_404
 
-# Create your tests here.
+from CV.views import CV, section_detail, section_edit
+from blog.views import post_list
+from CV.models import Section
+
+
+class CVPageTest(TestCase):
+
+    def test_root_url_resolves_to_CV_view(self):
+        found = resolve('/CV')  
+        self.assertEqual(found.func, CV)
+
+    def test_root_url_resolves_to_blog_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, post_list)
+    
+    def test_section_list_returns_correct_html(self):
+        response = self.client.get('/CV')
+        self.assertTemplateUsed(response, 'CV/CV_all_sections.html', 'CV/CV_base.html')
+
+    def test_CV_url_resolves_to_section_detail_view(self):
+        found = resolve('/section/1/')  
+        self.assertEqual(found.func, section_detail)
+ 
+    def test_section_detail_url_resolves_to_section_edit_view(self):
+        found = resolve('/section/1/edit/')  
+        self.assertEqual(found.func, section_edit)
+
+    def test_correctly_read_from_database_on_homepage(self):
+        Section.objects.create(header='Personal Details')
+        Section.objects.create(header='Education')
+        Section.objects.create(header='Experience')
+
+        response = self.client.get('/CV')
+
+        self.assertIn('Personal Details', response.content.decode())
+        self.assertIn('Education', response.content.decode())
+        self.assertIn('Experience', response.content.decode())
+
+
+        
+"""    
+    def test_can_update_a_POST_request(self):
+        response = self.client.post('/CV', data= {'sections': Section})
+        self.assertIn('A new post', response.content.decode())
+        self.assertTemplateUsed(response, 'section_edit.html')
+
+
+    def test_section_edit_returns_correct_html(self):
+        response = self.client.get('/section/1/edit/')
+        self.assertTemplateUsed(response, 'CV/section_edit.html')
+
+    def test_section_detail_returns_correct_html(self):
+        response = self.client.get('/section/1/')
+        self.assertTemplateUsed(response, 'CV/section_detail.html')
+        """
--- testing/functional_tests.py
+++ testing/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):  
@@ -9,17 +10,57 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_opens_blogs_home_page(self):  
-        self.browser.get('http://localhost:8000')
+    def test_opens_blog_home_page_and_browses(self):  
+        #user opens blog home page
+        self.browser.get('http://dsb874.pythonanywhere.com/')
 
-        self.assertIn('Blog', self.browser.title) 
-
+        #they expect the correct header and title to be shown
+        self.assertIn("Davinder's Blog", self.browser.title) 
         header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('Blog', header_text)
+        self.assertIn("Davinder's Blog", header_text)    
+
+        #they then move on to the CV page
+        self.opens_CV_page()
+
+        #this also needs the correct header and title
+        self.assertIn("Davinder's CV", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn("Davinder's CV", header_text)
+
+        #they also see an option to return to the Blog site
+        header_text = self.browser.find_element_by_tag_name('h3').text  
+        self.assertIn("Davinder's Blog", header_text)
+
+        #they test this link
+        self.browser.get('http://dsb874.pythonanywhere.com/')
+
+        #they then move back to the CV page
+        self.opens_CV_page()
+
+        #they click on a section of the CV expect to see the details
+        self.browser.get('http://dsb874.pythonanywhere.com/section/1/')
+
+        #they click edit and expect to see the option to edit the section
+        self.browser.get('http://dsb874.pythonanywhere.com/section/1/edit/')
+
+        #they then move back to the CV page
+        self.opens_CV_page()
+
+        #they click on another section of the CV expect to see the details
+        self.browser.get('http://dsb874.pythonanywhere.com/section/2/')
+
+        #they click edit and expect to see the option to edit the section
+        self.browser.get('http://dsb874.pythonanywhere.com/section/2/edit/')
+
+        time.sleep(3)
 
 
-        #self.fail('Finish the test!')  
 
+    def opens_CV_page(self):
+        self.browser.get('http://dsb874.pythonanywhere.com/CV')
+
+# def test_opens_CV_section_detail(self):
+# def test_opens_CV_section_edit(self):
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore') 
